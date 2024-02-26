b4622341d73a5ac32f9b10c9467b8dcb7939c0d2
Leo Cheung <leozhang621@gmail.com>
2024-02-26 19:13:55 +0800

    feat: add prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 
+const baseOutput = { dir: './dist', entryFileNames: '[name].[format].js' };
 /**
  * @type {import('rollup').RollupOptions}
  */
 const config = {
   input: ['src/cli.ts'],
   output: [
-    { format: 'cjs', file: 'dist/index.cjs.js' },
-    { format: 'esm', file: 'dist/index.esm.js' },
+    { format: 'cjs', ...baseOutput },
+    { format: 'esm', ...baseOutput },
   ],
-  plugins: [
-    typescript(),
-    json(),
-  ],
+  plugins: [typescript(), json()],
 };
 
 export default config;
